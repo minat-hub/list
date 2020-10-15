d433b88be7264f25be4e9306f1cf20612f225f8d
mina <mina3504ever@gmail.com>
2020-10-14 18:05:01 -0700

    changes made to project

---
+++ herokuapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('delete/<list_id>', views.delete, name="delete"),
+    path('patient_not_attended_to/<list_id>', views.patient_not_attended_to, name="patient_not_attended_to"),
+    path('patient_attended_to', views.patient_attended_to, name="patient_attended_to"),
+    path('edit/<list_id>', views.edit, name="edit"),
+]
