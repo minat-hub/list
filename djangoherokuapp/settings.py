6585442f3c7f22d5685627e9c0ab9526878f675f
mina <mina3504ever@gmail.com>
2020-10-22 13:58:04 -0700

    new changes

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'xjnt$(x@k0z%1^y(g9x_llc745ijj9i4xqm8_t-p5d5x(#4d5p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokudjangoappp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
